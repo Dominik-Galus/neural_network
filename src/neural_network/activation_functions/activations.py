739f865bd3e69fe08ecf88fc01c137efe697b604
Dominik Galus <dominikgalus9@gmail.com>
2025-03-07 21:21:56 +0100

    feat: add activation functions to layer and fix the type hinting

--- src/neural_network/activation_functions/activations.py
+++ src/neural_network/activation_functions/activations.py
@@ -1,14 +1,16 @@
 from collections.abc import Callable
 
+import numpy as np
+
 from neural_network.activation_functions.relu import relu_activation, relu_derivative
 from neural_network.activation_functions.sigmoid import sigmoid_activation, sigmoid_derivative
 
-ACTIVATION_FUNCTIONS: dict[str, Callable] = {
+ACTIVATION_FUNCTIONS: dict[str, Callable[[np.ndarray], np.ndarray]] = {
     "relu": relu_activation,
     "sigmoid": sigmoid_activation,
 }
 
-ACTIVATION_DERIVATIVES: dict[str, Callable] = {
+ACTIVATION_DERIVATIVES: dict[str, Callable[[np.ndarray], np.ndarray]] = {
     "relu": relu_derivative,
     "sigmoid": sigmoid_derivative,
 }
--- src/neural_network/layer.py
+++ src/neural_network/layer.py
@@ -1,4 +1,8 @@
+from collections.abc import Callable  # noqa: TC003
+
 import numpy as np
+
+from neural_network.activation_functions.activations import ACTIVATION_DERIVATIVES, ACTIVATION_FUNCTIONS
 
 
 class Layer:
@@ -11,12 +15,20 @@ class Layer:
         activation_function: str = "relu",
         l2: int = 0,
     ) -> None:
-        self.activation_function = activation_function
+        self.activation_function: Callable[[np.ndarray], np.ndarray] | None = ACTIVATION_FUNCTIONS[activation_function]
+        self.activation_derivative: Callable[[np.ndarray], np.ndarray] | None = ACTIVATION_DERIVATIVES[
+            activation_function
+        ]
+        if self.activation_function is None or self.activation_derivative is None:
+            msg = "There is no such activation function available"
+            raise TypeError(msg)
+
         self.neurons = neurons
         self.learning_rate = learning_rate
         self.inputs = inputs
         self.l2 = l2
         self.weights = np.random.default_rng(random_state).normal(loc=0, scale=0.1, size=(inputs, neurons))
+        self.bias = np.ones(shape=neurons)
 
     def forward(self, x: np.ndarray) -> None:
         pass
